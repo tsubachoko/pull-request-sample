9690749449304238d56916835e0a08b5640cf45c
yamasaki <yamasaki@bekewl.com>
2022-10-17 14:40:32 +0900

    配列をソートする処理を追加しました

--- sort.js
+++ sort.js
@@ -1,7 +1,6 @@
-var number = [19, 3, 81, 1, 24, 21];
 var sortNumber = function (number) {
   number.sort(function (a, b) {
-      if (a === b) {
+      if (a == b) {
           return 0;
       }
       return a < b ? -1 : 1;
